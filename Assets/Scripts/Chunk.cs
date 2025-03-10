e22d3e7827f5d259b40fd008f3b857724474d0b7
keyyzo <58720632+keyyzo@users.noreply.github.com>
2025-03-10 00:05:53 +0000

    Added pickups for apples and coins, customised the coins spawning algorithm, currently implementing destroying obstacles that are out of bounds

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,22 +1,128 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
+    [Header("Prefab Variables")]
+
     [SerializeField] GameObject fencePrefab;
+    [SerializeField] GameObject applesPrefab;
+    [SerializeField] GameObject coinsPrefab;
+
+    [Space(10)]
+
+    [Header("Spawn Chance Variables")]
+
+    [SerializeField] float appleSpawnChance = 0.33f;
+    [SerializeField] float coinSpawnChance = 0.5f;
+
+    [Space(10)]
+
+    [Header("Lane Variables")]
+
     [SerializeField] float[] lanes = { -2.5f, 0f, 2.5f};
+    [SerializeField] float[] coinSpawnPositions = { -4f, -2f, 0f, 2f, 4f };
+    [SerializeField] float coinSeperationLength = 2f;
 
-
+    List<int> availableLanes = new List<int> { 0,1,2 };
+    List<int> availableCoinPositions = new List<int> { 0, 1, 2, 3, 4 };
 
     private void Start()
     {
         SpawnFence();
+        SpawnApple();
+        SpawnCoins();
     }
 
     void SpawnFence()
     {
-        int randomLaneIndex = Random.Range(0, lanes.Length);
-        Vector3 spawnPosition = new Vector3(lanes[randomLaneIndex], transform.position.y, transform.position.z);
 
-        Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+        int fencesToSpawn = Random.Range(0, lanes.Length);
+
+        for (int i = 0; i < fencesToSpawn; i++)
+        {
+
+            if (availableLanes.Count <= 0)
+                break;
+
+            int selectedLane = SelectLane();
+
+            Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
+
+            Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+        }
+
+
     }
+
+
+    void SpawnApple()
+    {
+        if (Random.value > appleSpawnChance)
+            return;
+
+        if (availableLanes.Count <= 0)
+            return;
+
+        int selectedLane = SelectLane();
+
+        Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
+
+        Instantiate(applesPrefab, spawnPosition, Quaternion.identity, this.transform);
+    }
+
+
+    void SpawnCoins()
+    {
+        if (Random.value > coinSpawnChance)
+            return;
+
+        if (availableLanes.Count <= 0)
+            return;
+
+        if(availableCoinPositions.Count <= 0)
+            return;
+
+        int selectedLane = SelectLane();
+
+        int maxCoinsToSpawn = 6;
+        int coinsToSpawn = Random.Range(1, maxCoinsToSpawn);
+
+        float topOfChunkZPos = transform.position.z + (coinSeperationLength * 2f);
+
+        for (int i = 0; i < coinsToSpawn; i++)
+        {
+            //int selectedPos = SelectCoinPosition();
+
+            float spawnPositionZ = topOfChunkZPos - (i * coinSeperationLength);
+
+            Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, spawnPositionZ);
+
+            Instantiate(coinsPrefab, spawnPosition, Quaternion.identity, this.transform);
+        }
+
+        
+    }
+
+    int SelectLane()
+    {
+        int randomLaneIndex = Random.Range(0, availableLanes.Count);
+        int selectedLane = availableLanes[randomLaneIndex];
+        availableLanes.RemoveAt(randomLaneIndex);
+        return selectedLane;
+    }
+
+    // My own solution to the problem of spawning multiple coins within a lane on the chunk
+    // Have stopped using it due to following the course, however keeping the code intact
+    // incase of a potential resuse for it
+    int SelectCoinPosition()
+    {
+        int randomPosIndex = Random.Range(0, availableCoinPositions.Count);
+        int selectedPos = availableCoinPositions[randomPosIndex];
+        availableCoinPositions.RemoveAt(randomPosIndex);
+        return selectedPos;
+    }
+
+    
 }
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    const string playerString = "Player";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerString))
+        {
+            Debug.Log(other.gameObject.name);
+        }
+        
+    }
+}
